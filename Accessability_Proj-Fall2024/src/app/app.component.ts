b10717071865c2290b68e42549d2005aaec04943
rokusho723 <rokusho723@gmail.com>
2024-10-01 14:14:57 -0500

    Project Initialized
    Initialized angular project

---
+++ Accessability_Proj-Fall2024/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Accessability_Proj-Fall2024';
+}
